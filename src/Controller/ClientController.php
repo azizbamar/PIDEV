16f91d064ecd8023440316ed363973d996e79e70
yasmine <yasmin.jenhani@gmail.com>
2024-02-07 18:11:08 +0100

    pushing template

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use symfony\Component\HttpFoundation\RedirectResponse;
 
 class ClientController extends AbstractController
 {
@@ -13,10 +12,5 @@ class ClientController extends AbstractController
     public function client(): Response
     {
         return $this->render('client/index.html.twig');
-    }
-    #[Route('/homepage', name: 'app_homepage')]
-    public function homepage(): Response
-    {
-        return $this->render('client/homepage.html.twig');
     }
 }
