62d3386ce7afb9eb0bee2923e80e7dc25ed06bfe
azizbamar <azizbamar16@gmail.com>
2024-02-07 16:28:48 +0100

    fixed templates

---
+++ src/Controller/ClientController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ClientController extends AbstractController
+{
+    #[Route('/client', name: 'app_client')]
+    public function client(): Response
+    {
+        return $this->render('client/index.html.twig');
+    }
+}
