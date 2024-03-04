840ef77f46a4222e59fcb9c5d19e368ea7778c8f
oussema ayari <oussemaa782@gmail.com>
2024-03-04 07:53:29 +0100

    final commit

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,11 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    VictorPrdh\RecaptchaBundle\RecaptchaBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
 ];
--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
+use MercurySeries\FlashyBundle\FlashyNotifier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,4 +15,18 @@ class ClientController extends AbstractController
     {
         return $this->render('client/index.html.twig');
     }
+    #[Route('/clienta', name: 'app_clienta')]
+    public function clienta(ArticleRepository $articleRepository,FlashyNotifier $flashy ): Response
+    {
+
+        // $flashy->success('explorer notre articles !', 'http://your-awesome-link.com');
+        return $this->render('client/clienta.html.twig',[
+            'articles' => $articleRepository->findAll(),
+        ]);
+    }
+    #[Route('/HomePage', name: 'Home_Page')]
+            public function HomePage(): Response
+            {
+                return $this->render('client/HomePage.html.twig');
+            }
 }
